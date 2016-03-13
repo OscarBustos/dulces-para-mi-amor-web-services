2de6e2a376eadd1e96cd71910e0b913158a5e696
Osarez7 <osarez7@gmail.com>
2016-03-12 21:58:43 -0500

    web params names added

--- src/main/java/org/chamerling/heroku/service/SumaService.java
+++ src/main/java/org/chamerling/heroku/service/SumaService.java
@@ -1,8 +1,11 @@
 package org.chamerling.heroku.service;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
-
+import javax.jws.WebResult;
 @WebService
+
 public interface SumaService {
-  public float sumar(float a, float b);
+  @WebResult(name="resultado")	
+  public float sumar(@WebParam(name="a")float a,@WebParam(name="b") float b);
 }
